3fa6f27dec16e4829c175ec36613b7f49ad7ec3e
jang9003 <103184038+jang9003@users.noreply.github.com>
2022-09-01 10:42:21 +0900

    Update GameManager.cs

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,12 +4,9 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-<<<<<<< HEAD
 
-    Vector3 b = new Vector3(3, 3, 3);
-=======
     Vector3 a = new vector3(3,3,3);
->>>>>>> 864d796955450fad63e1f2984ff28fe1031e71d0
+
     // Start is called before the first frame update
     void Start()
     {
