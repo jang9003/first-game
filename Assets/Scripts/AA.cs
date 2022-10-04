495172ecdb64de1845462779f49ae1378f658cd8
jang9003 <103184038+jang9003@users.noreply.github.com>
2022-10-04 23:56:11 +0900

    ㅁㄴㅇ

--- Assets/Scripts/AA.cs
+++ Assets/Scripts/AA.cs
@@ -7,7 +7,11 @@ public class AA : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+<<<<<<< Updated upstream
         Debug.Log("¾È³çÇÏ¼¼¿ä");
+=======
+        Debug.Log("aaaaaaa");
+>>>>>>> Stashed changes
     }
 
     // Update is called once per frame
